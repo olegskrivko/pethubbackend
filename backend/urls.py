0991618438ecf6ec9e2143f8f39e4bd689f8fddc
Olegs <olegs.krivko@gmail.com>
2025-06-11 12:18:27 +0300

    fix url

--- backend/urls.py
+++ backend/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('api/chatbot/', include('chatbot.urls')),
     path('api/feedbacks/', include('feedback.urls')),
     path('api/notifications/', include('notifications.urls')),
-    path('api/payments/', include('payment.urls')),
+    path('api/payment/', include('payment.urls')),
     path('api/pets/', include('pets.urls')),
     path('api/services/', include('services.urls')),
     path('api/shelters/', include('shelters.urls')),
